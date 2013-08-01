2bf687f2ecdfa8d6800448e3885fa13132d171b9
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-08-01 23:22:51 +0200

    CellStack#push should discard oldest elements when overflowing

--- lib/CellStack.rb
+++ lib/CellStack.rb
@@ -1,11 +1,13 @@
 class CellStack
-  def initialize
+  def initialize size
+    @size = size
     @elements = []
   end
   def pop
     @elements.pop
   end
   def push val
+    @elements.shift if @elements.count == @size
     @elements.push val
   end
 end
--- spec/CellStack_spec.rb
+++ spec/CellStack_spec.rb
@@ -1,16 +1,27 @@
 require 'CellStack'
 
 describe :CellStack do
+  before(:each) do
+    @stack = CellStack.new(10)
+  end
   context "#pop" do
     it "should return nil for an empty stack" do
-      stack = CellStack.new.pop
+      @stack.pop.should be_nil
+    end
+  end
+  context "#push" do
+    it "should discard oldest elements when overflowing" do
+      stack = CellStack.new(2)
+      stack.push :OLD
+      2.times { stack.push :VAL }
+      2.times { stack.pop.should be(:VAL) }
+      stack.pop.should be_nil
     end
   end
   it "should pop pushed values in reverse order" do
-    stack = CellStack.new
-    stack.push :FIRST
-    stack.push :SECOND
-    stack.pop.should be(:SECOND)
-    stack.pop.should be(:FIRST)
+    @stack.push :FIRST
+    @stack.push :SECOND
+    @stack.pop.should be(:SECOND)
+    @stack.pop.should be(:FIRST)
   end
 end
