7aaf4153d8dbee683352b3e6d47a4f49129e2c07
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-08-01 23:48:47 +0200

    CellMemory should ignore out of range writes

--- lib/CellMemory.rb
+++ lib/CellMemory.rb
@@ -1,11 +1,12 @@
 class CellMemory
   def initialize size
-    @values = []
+    @values = Array.new(size, 0)
   end
   def storeValueAt val, addr
-    @values[addr] = val
+    @values[addr] = val if addr < @values.size && addr >= 0
   end
   def loadFrom addr
-    @values[addr] || 0
+    return 0 if addr < 0 || addr >= @values.size 
+    @values[addr]
   end
 end
--- spec/CellMemory_spec.rb
+++ spec/CellMemory_spec.rb
@@ -5,9 +5,9 @@ describe :CellMemory do
     @memory = CellMemory.new(10)
   }
   it "should store values at given addresses" do
-    @memory.storeValueAt 44, 3
+    @memory.storeValueAt 44, 0
     @memory.storeValueAt 55, 9
-    @memory.loadFrom(3).should be(44)
+    @memory.loadFrom(0).should be(44)
     @memory.loadFrom(9).should be(55)
   end
   it "should initialize elements with 0" do
@@ -18,5 +18,12 @@ describe :CellMemory do
     @memory.loadFrom(-1).should eq(0)
     @memory.loadFrom(10).should eq(0)
     @memory.loadFrom(11).should eq(0)
+  end
+  it "should ignore out of range writes" do
+    @memory.storeValueAt :BAD, 9
+    @memory.storeValueAt 33, -1
+    @memory.storeValueAt 99, 10
+    @memory.loadFrom(-1).should eq(0)
+    @memory.loadFrom(10).should eq(0)
   end
 end
